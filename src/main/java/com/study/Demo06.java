6907d3b47cf808e927bc33d2c7e9a6df97d33aee
Gu <294403373@qq.com>
2019-09-22 10:40:36 +0900

    Demo 提交

--- src/main/java/com/study/Demo06.java
+++ src/main/java/com/study/Demo06.java
@@ -1,4 +1,20 @@
 package com.study;
 
+import java.io.IOException;
+
 public class Demo06 {
+    public static void main(String[] args) {
+        X x = new X();
+        try {
+            x.printFileContent();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
+class X {
+    public void printFileContent() throws IOException {
+        throw new IOException();
+    }
+}
+
