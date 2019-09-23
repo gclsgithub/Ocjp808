01c914f716f44ba259bf6c287cb82973fcfe646d
Gu <294403373@qq.com>
2019-09-23 22:37:34 +0900

    No17

---
+++ src/main/java/com/study/Demo17.java
@@ -0,0 +1,20 @@
+package com.study;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class Demo17 {
+    public static void main(String[] args) {
+        LocalDate date1 = LocalDate.now();
+
+        LocalDate date2 = LocalDate.of(2014,6,20);
+
+        LocalDate date3 = LocalDate.parse("2014-06-20", DateTimeFormatter.ISO_DATE);
+
+        System.out.println(date1);
+
+        System.out.println(date2);
+
+        System.out.println(date3);
+    }
+}
